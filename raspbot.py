b6cd1350b1ae1c53acb8e5ce382271b5aadd585c
Ron V <ron@evals.net>
2015-03-13 07:38:26 +0000

    hello world

--- raspbot.py
+++ raspbot.py
@@ -1,13 +1,13 @@
-
-
-
-
-
-
-
-
-
-
-
-
 #Python version 3.2.3
+
+#array = ["Steve Perry", "Chuck Norris"]
+#if array[0] > array[1]:
+#    print("you're a towel")
+#else:
+#    print("what no wai")
+
+#text = "?"
+#while not(text == ""):
+#    text = input("Type something: ")
+#    print(text)
+    
