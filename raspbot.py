a91eec0dbb3b3b51d821b2667c71b96f0124a1ed
Ron V <ron@evals.net>
2015-03-14 22:26:09 +0000

    asdf

--- raspbot.py
+++ raspbot.py
@@ -7,11 +7,17 @@ import time
 
 
 class RaspBot:
-    gpio = [12,16,21,19]
+    gpio = [20,21,22,23,24,25,26,27]
+    timeout = 0.2
+    max_loops = 10
 
-    def reset_lights(self):
+    def all_off(self):
         for val in self.gpio:
             io.output(val, False)
+
+    def all_on(self):
+        for val in self.gpio:
+            io.output(val, True)
 
     def __init__(self):
         io.setmode(io.BCM)
@@ -22,24 +28,63 @@ class RaspBot:
     def keyboard_loop(self):
         key = ""
         while not(key == "q"):
-            key = input("Command (q)uit (a) (r)eset: ")
-            print(key)
-            if key == 'a':
+            key = input("Command (q)uit (l)oop (a)ll (o)ff (r)eset (f)orward (b)ackward (1)BackAndForth (2)LoopForward (3)LoopBackward (4)BackForthEven (5)BackForthOdd: ")
+            if key == 'l':
                 self.loop_em()
-            if key == 'r':
-                self.reset_lights()
+            if key == 'a':
+                self.all_on()
+            if key == 'o':
+                self.all_off()
+            if key == 'f':
+                self.run_forward()
+            if key == 'b':
+                self.run_backward()
+            if key == '1':
+                self.back_forth()
+            if key == '2':
+                self.loop_forward()
+            if key == '3':
+                self.loop_backward()
+            if key == '4':
+                self.back_forth('even')
+            if key == '5':
+                self.back_forth('odd')
 
-    def loop_em(self):
-        self.reset_lights()
-        count = 0
-        while True:
-            self.reset_lights()
-            io.output(self.gpio[ count ], True)
-            if count == 3:
-                count = 0
-            else:
-                count = int(count) + 1
-            time.sleep(.2)
+    def loop_forward(self, type = 'all'):
+        self.all_off()
+        for x in range(1, self.max_loops):
+            self.run_forward(type)    
+
+    def loop_backward(self, type = 'all'):
+        self.all_off()
+        for x in range(1, self.max_loops):
+            self.run_backward(type)	
+
+    def back_forth(self, type = 'all'):
+        self.all_off()
+        for x in range(1, self.max_loops):
+            self.run_forward(type)
+            self.run_backward(type)
+
+    def run_forward(self, type = 'all'):
+        for var in self.gpio:
+            if (type == 'even') and (var % 2 == 0):
+                continue
+            if (type == 'odd') and (not(var % 2 == 0)):
+                continue
+            io.output(var, True)
+            time.sleep(self.timeout)
+            io.output(var, False)
+        
+    def run_backward(self, type = 'all'):
+        for var in reversed(self.gpio):
+            if (type == 'even') and (var % 2 == 0):
+                continue
+            if (type == 'odd') and (not(var % 2 == 0)):
+                continue
+            io.output(var, True)
+            time.sleep(self.timeout)
+            io.output(var, False)
 
 pie = RaspBot()
 
